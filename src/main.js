c7bce3045ba06aa1fb25e9dceda6f4a50ae8b2e5
tropswhy <solodkovnikita08@gmail.com>
2024-01-30 14:15:12 +0300

    initial vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
-import './style.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const pinia = createPinia()
 
@@ -17,4 +25,4 @@ const router = createRouter({
     routes,
 })
 
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App).use(vuetify).use(pinia).use(router).mount('#app')
