6cb93316662dd2f576da3b7183768e0f963b0cd4
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 11:41:04 +0300

    add addPayerId and updateWhoAteId(not finished) functions

--- src/stores/ProductsStore.js
+++ src/stores/ProductsStore.js
@@ -1,8 +1,32 @@
 import { defineStore } from 'pinia'
+import { usePeopleStore } from './PeopleStore.js'
+
 
 export const useProductsStore = defineStore('ProductsStore', {
     state: () => ({
-        list: [],
+        list: [
+            {
+                id: 1,
+                name: 'Водка',
+                price: '500',
+                payerId: 15645667,
+                whoAte: [{1: false}, {2: true}, {3: false}, {4: true}, {5: false},],
+            },
+            {
+                id: 2,
+                name: 'Пиво',
+                price: '150',
+                payerId: 2,
+                whoAte: [{1: false}, {2: false}, {3: true}, {4: true}, {5: false},],
+            },
+            {
+                id: 3,
+                name: 'Сигареты',
+                price: '230',
+                payerId: 3,
+                whoAte: [{1: true}, {2: true}, {3: false}, {4: true}, {5: false},],
+            },
+        ],
     }),
     actions: {
         addProd(name, price) {
@@ -18,7 +42,10 @@ export const useProductsStore = defineStore('ProductsStore', {
                         id: Date.now(),
                         name: name,
                         price: price,
-                    },
+                        payerId: -1,
+                        whoAte: '',
+                        },
+                    
                 ]
             } else {
                 alert(
@@ -29,8 +56,8 @@ export const useProductsStore = defineStore('ProductsStore', {
 
         findById(id) {
             let ind = -1
-            this.list.map(
-                (item, index) => (item.id = id ? (ind = index) : (ind = ind))
+            this.list.map((item, index) =>
+                item.id === id ? (ind = index) : (ind = ind)
             )
             if (ind == -1) return undefined
             return this.list[ind]
@@ -43,8 +70,21 @@ export const useProductsStore = defineStore('ProductsStore', {
         sum() {
             let sum = this.list.reduce(function (accumulator, currentValue) {
                 return Number(accumulator) + Number(currentValue.price)
-            }, 0);
+            }, 0)
             return sum
         },
+
+        addPayerId(payerId, prodId) {
+            console.log(prodId)
+            this.findById(prodId)['payerId'] = payerId
+        },
+    
+        updateWhoAteId(productId, personId){
+            this.findById(productId).whoAte[personId] = !product.whoAte[personId] 
+            this.list.map((product) => {
+                if (product.id === productId) {
+                }
+            })
+        }
     },
 })
