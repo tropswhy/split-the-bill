e524a38db60476a82eb2ba8ca82760d160028945
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 16:53:27 +0300

    add AppBar and card

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,12 +1,17 @@
 <script setup>
 import AppBar from '../components/ui/AppBar.vue'
-
 </script>
 
 <template>
-<AppBar/>
+    <v-app>
+        <AppBar />
+            <v-card
+                color="blue-lighten-5"
+                width="500"
+                class="text-center mx-auto"
+                title="Результаты"
+                text="Да! Да! Ура! Победа!" />
+    </v-app>
 </template>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang="scss" scoped></style>
