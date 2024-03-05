b3cc8d0d3bf7547a35081ffc6cd68d8bb93fad9d
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 15:49:56 +0300

    add margin bottom

--- src/components/ui/NextButton.vue
+++ src/components/ui/NextButton.vue
@@ -2,7 +2,7 @@
     <v-btn
         rounded
         color="primary"
-        class="mx-auto mt-5"
+        class="mx-auto mt-5 mb-5"
         width="350"
         prepend-icon="mdi-arrow-right-thin-circle-outline">
         <slot></slot>
