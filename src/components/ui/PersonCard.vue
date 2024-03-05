91350c807f1ec122457a56ef10d315a9be1e6ca3
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 11:38:55 +0300

    add PersonCard component

---
+++ src/components/ui/PersonCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps(['name', 'checked'])
+const checked = ref(props.checked)
+</script>
+
+<template>
+    <div class="card bg-blue-lighten-4">
+        <v-checkbox
+            v-model="checked"
+            density="comfortable"
+            @update:modelValue=""
+            />
+        <p>
+            {{ `${name.slice(0, 10)}${name.length > 9 ? '...' : ''}` }}
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 10px;
+    padding: 12px;
+    min-width: 150px;
+}
+</style>
