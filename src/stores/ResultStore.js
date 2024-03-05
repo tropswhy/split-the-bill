b5c2c87aa6d580ec0007629fb8d47d9493273090
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 16:53:44 +0300

    make ResultStore

---
+++ src/stores/ResultStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export const useResultStore = defineStore("resultStore",{
+    state: () => ({
+        results: [],
+    }),
+
+    actions: {
+        
+    }
+})
