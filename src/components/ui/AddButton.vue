4328176b12c441ca172f6d8636eb94d9e6a7a4d7
tropswhy <solodkovnikita08@gmail.com>
2024-03-05 15:36:40 +0300

    delete Button component and add AddButton and NextButton

---
+++ src/components/ui/AddButton.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn
+        rounded
+        color="primary"
+        prepend-icon="mdi-plus-circle-outline">
+        <v-icon v-slot:prepend></v-icon>
+        <slot></slot>
+    </v-btn>
+</template>
