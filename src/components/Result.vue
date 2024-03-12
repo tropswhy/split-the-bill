04d7c877a61bf9b74bca2593207de10fe9c7280c
tropswhy <solodkovnikita08@gmail.com>
2024-03-12 20:09:54 +0300

    finish result store and result page

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,17 +1,67 @@
 <script setup>
 import AppBar from '../components/ui/AppBar.vue'
+import { usePersonStore } from '../stores/PersonStore'
+import { useProductStore } from '../stores/ProductStore'
+import { useResultStore } from '../stores/ResultStore'
+
+const PersonStore = usePersonStore()
+const ProductStore = useProductStore()
+const ResultStore = useResultStore()
+
+const personIds = PersonStore.list.reduce(
+    (accum, item) => [...accum, item.id],
+    []
+)
+
+ResultStore.countResultForPayer(PersonStore, ProductStore, personIds)
+ResultStore.checkForDebts()
+ResultStore.debtRound()
 </script>
 
 <template>
     <v-app>
         <AppBar />
-            <v-card
-                color="blue-lighten-5"
+        <v-card
+            color="blue-lighten-5"
+            width="500"
+            class="text-center mx-auto"
+            title="Результаты"
+            text="Да! Да! Ура! Победа!" />
+
+        <v-list>
+            <v-list-item
+                v-for="result in ResultStore.resultForPayer"
+                :key="result.payerId">
+                <v-card class="text-center mx-auto my-auto"
+                color="primary"
                 width="500"
-                class="text-center mx-auto"
-                title="Результаты"
-                text="Да! Да! Ура! Победа!" />
+                borders="10"
+                v-if="result.debters">
+                        Пользователю
+                        {{ PersonStore.findById(result.payerId).name }}
+                        должны
+
+                    <!-- <v-list
+                        v-for="person in PersonStore.list"
+                        :key="person.id">
+                        <v-list-item :class="(result.debters[person.id] > 1) ? 'visible' : 'non-visible'">
+                            Пользователь
+                            {{ PersonStore.findById(person.id).name }}
+                            {{ result.debters[person.id] }} рублей
+                        </v-list-item> -->
+                        <v-list
+                        v-for="person in PersonStore.list"
+                        :key="person.id">
+                        <v-list-item
+                        v-if="result.debters[person.id] !== undefined">
+                            Пользователь
+                            {{ PersonStore.findById(person.id).name }}
+                            {{ result.debters[person.id] }} рублей
+                        </v-list-item>
+                    </v-list></v-card
+                >
+            </v-list-item>
+        </v-list>
     </v-app>
 </template>
 
-<style lang="scss" scoped></style>
--- src/stores/ResultStore.js
+++ src/stores/ResultStore.js
@@ -1,11 +1,111 @@
 import { defineStore } from 'pinia'
 
-export const useResultStore = defineStore("resultStore",{
+export const useResultStore = defineStore('ResultStore', {
     state: () => ({
-        results: [],
+        resultForPayer: [],
     }),
 
     actions: {
-        
-    }
+        countResultForPayer(persons, products, personIds) {
+            products.list.forEach((prod) => {
+                let payer = persons.findById(prod.payerId)
+
+                if (this.resultForPayer.find((result) => result.payerId === payer.id) === undefined)
+                    this.addPayer(payer.id, personIds, prod.whoAte)
+
+                for (let whoAteId in prod.whoAte) {
+                    if (this.findByPayerId(payer.id).debters[whoAteId] > 0)
+                        this.findByPayerId(payer.id).debters[whoAteId] = this.findByPayerId(payer.id).debters[whoAteId] + prod.price / prod.amountOfEaters
+                    else if (
+                        whoAteId != payer.id &&
+                        prod.whoAte[whoAteId] === true
+                    )
+                        this.findByPayerId(payer.id).debters[whoAteId] = prod.price / prod.amountOfEaters
+                }
+            })
+        },
+
+        addPayer(payerId, personIds, whoAte) {
+            let zeroDebt = {}
+            personIds.map((personId) => {
+                if (personId != payerId && whoAte[personId] === true)
+                    zeroDebt[personId] = 0
+            })
+            this.resultForPayer = [
+                ...this.resultForPayer,
+                {
+                    payerId: payerId,
+                    debters: '',
+                },
+            ]
+
+            this.resultForPayer.at(-1).debters = {
+                ...this.resultForPayer.at(-1).debters,
+                ...zeroDebt,
+            }
+        },
+
+        findByPayerId(id) {
+            let ind = -1
+            this.resultForPayer.map((item, index) =>
+                item.payerId === id ? (ind = index) : (ind = ind)
+            )
+            if (ind == -1) return undefined
+            return this.resultForPayer[ind]
+        },
+
+        checkForDebts() {
+            this.resultForPayer.map((result) => {
+                this.resultForPayer.forEach((resultForDebtor) => {
+                    if (
+                        resultForDebtor.debters[result.payerId] !== undefined &&
+                        result.debters[resultForDebtor.payerId] !== undefined
+                    ) {
+                        if (
+                            resultForDebtor.debters[result.payerId] >
+                            result.debters[resultForDebtor.payerId]
+                        ) {
+                            resultForDebtor.debters[result.payerId] -=
+                                result.debters[resultForDebtor.payerId]
+                            delete result.debters[resultForDebtor.payerId]
+                        } else if (
+                            resultForDebtor.debters[result.payerId] <
+                            result.debters[resultForDebtor.payerId]
+                        ) {
+                            result.debters[resultForDebtor.payerId] -=
+                                resultForDebtor.debters[result.payerId]
+                            delete resultForDebtor.debters[result.payerId]
+                        } else if (
+                            resultForDebtor.debters[result.payerId] ===
+                            result.debters[resultForDebtor.payerId]
+                        ) {
+                            delete result.debters[resultForDebtor.payerId]
+                            delete resultForDebtor.debters[result.payerId]
+                        }
+
+                        if (Object.keys(resultForDebtor.debters).length === 0) {
+                            let index = this.resultForPayer.indexOf(this.findByPayerId(resultForDebtor.payerId))
+                            this.resultForPayer.splice(index, 1)
+                        }
+
+                        if (Object.keys(result.debters).length === 0) {
+                            let index = this.resultForPayer.indexOf(
+                                this.findByPayerId(result.payerId)
+                            )
+                            this.resultForPayer.splice(index, 1)
+                        }
+                    }
+                })
+            })
+        },
+
+        debtRound() {
+            this.resultForPayer.forEach((result) => {
+                let debterIds = Object.keys(result.debters)
+                debterIds.forEach((index) => {
+                    result.debters[index] = Math.round(result.debters[index] * 100) / 100
+                })
+            })
+        },
+    },
 })
