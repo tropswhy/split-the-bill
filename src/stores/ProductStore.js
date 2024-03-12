eb41b82a5e0f4f39770e22a18cdef1c7524d0fe9
tropswhy <solodkovnikita08@gmail.com>
2024-03-12 20:05:26 +0300

    add property amountOfEaters

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -3,27 +3,30 @@ import { defineStore } from 'pinia'
 export const useProductStore = defineStore('ProductStore', {
     state: () => ({
         list: [
-            {
+            /* {
                 id: 1,
                 name: 'Водка',
                 price: '500',
-                payerId: 15645667,
-                whoAte: {1: false, 2: false, 3: false, 4: false, 5: false},
+                payerId: 1,
+                whoAte: {1: true, 2: true, 3: false, 4: false, 5: false},
+                amountOfEaters: 2,
             },
             {
                 id: 2,
                 name: 'Пиво',
                 price: '150',
-                payerId: 2,
-                whoAte: {1: false, 2: false, 3: false, 4: false, 5: false},
+                payerId: 3,
+                whoAte: {1: false, 2: true, 3: false, 4: false, 5: true},
+                amountOfEaters: 3,
             },
             {
                 id: 3,
                 name: 'Сигареты',
                 price: '230',
-                payerId: 3,
-                whoAte: {1: false, 2: false, 3: false, 4: false, 5: false},
-            },
+                payerId: 2,
+                whoAte: {1: true, 2: true, 3: false, 4: false, 5: false},
+                amountOfEaters: 3,
+            }, */
         ],
     }),
     actions: {
@@ -37,7 +40,8 @@ export const useProductStore = defineStore('ProductStore', {
                 name != null &&
                 name.replaceAll(' ', '') !== '' &&
                 price != null &&
-                price.replaceAll(' ', '') !== ''
+                price.replaceAll(' ', '') !== '' &&
+                Number(price) > 0
             ) {
                 this.list = [
                     ...this.list,
@@ -47,6 +51,7 @@ export const useProductStore = defineStore('ProductStore', {
                         price: price,
                         payerId: -1,
                         whoAte: '',
+                        amountOfEaters: 0,
                         },
                     
                 ]
@@ -82,13 +87,16 @@ export const useProductStore = defineStore('ProductStore', {
         },
 
         addPayerId(payerId, prodId) {
-            console.log(prodId)
             this.findById(prodId)['payerId'] = payerId
         },
     
         updateWhoAteId(productId, personId, condition){
             let product =  this.findById(productId)
             product.whoAte[personId] = condition
+            if (condition === true)
+                product.amountOfEaters++
+            else 
+                product.amountOfEaters--
         }
     },
 })
